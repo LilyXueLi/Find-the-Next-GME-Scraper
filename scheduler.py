11344e889ba1252a94acdcf0d6fc9bcba588f3b8
Lily Li <lilydenris@gmail.com>
2021-07-10 17:54:34 -0400

    moved ranking change calculation to back-end

--- scheduler.py
+++ scheduler.py
@@ -9,7 +9,7 @@ import yfinance as yf
 TOTAL_NUMBER_TO_PRESENT = 10
 
 # Number of the most discussed stocks we save
-# in case Yahoo Finance is not able to provide information for certain stocks
+# This should be greater than 10, in case Yahoo Finance is not able to provide information for certain stocks
 TOTAL_NUMBER_OF_STOCKS = 20
 
 # A list of abbreviations that generally do not represent tickers.
@@ -32,8 +32,8 @@ def build_dictionary(file_name, ticker_dict):
                 ticker_dict[symbol] = name
 
 
-# Count the tickers appeared in the titles and/or contents of the hot posts
-# Returns a dictionary with top 10 counts in non-increasing order
+# Counts the tickers appeared in the titles and/or contents of the hot posts
+# Returns a dictionary with top 20 counts in non-increasing order
 def count_tickers(reddit, ticker_dict):
     ticker_count = {}
     hot_posts = reddit.subreddit('wallstreetbets').hot(limit=500)
@@ -71,17 +71,50 @@ def found_all_fields(stock_info):
     return True
 
 
-# Packages the top10 tickers and related financial information into dictionaries
-# Inserts this information into the database with time stamps
-def insert_to_db(sorted_ticker_dict, ticker_dict):
-    toDB = []
+# Helper function to get the most recent Top 10 stock record from the database
+# Returns a list of ticker symbols in non-decreasing order
+def get_prev_stock_list(collection, now):
+    prev_entry = collection.find({'timeStamp': {'$lte': now}}).sort([('timeStamp', -1)]).limit(1)
 
-    sorted_ticker_list = list(sorted_ticker_dict.keys())
+    prev_time_stamp = prev_entry[0].get('timeStamp')
+    prev_stock_obj_list = collection.find({'timeStamp': prev_time_stamp})
+
+    prev_stock_list = []
+    for stock_obj in prev_stock_obj_list:
+        prev_stock_list.append(stock_obj.get('ticker'))
+
+    return prev_stock_list
+
+
+# Helper function to calculate ranking changes for the current Top 10 stocks
+# returns a tuple of whether this stock is new on the list and the ranking change (if applicable)
+def get_ranking_change(prev_stock_list, output_ticker, to_DB):
+    is_new = False
+    if output_ticker not in prev_stock_list:
+        is_new = True
+
+    ranking_change = 0
+    if not is_new:
+        ranking_change = len(to_DB) - prev_stock_list.index(output_ticker)
+
+    return is_new, ranking_change
+
+
+# Packages the Top 10 tickers and related financial information into a list of dictionaries
+# also inserts this information into the database with time stamps
+def insert_to_db(sorted_ticker_dict, ticker_dict):
+    client = MongoClient(os.environ['DB_URL'])
+    collection = client['findMyGME']['stocks']
 
     now = datetime.now()
 
-    for i in range(TOTAL_NUMBER_OF_STOCKS):
+    to_DB = []
 
+    sorted_ticker_list = list(sorted_ticker_dict.keys())
+
+    prev_stock_list = get_prev_stock_list(collection, now)
+
+    for i in range(TOTAL_NUMBER_OF_STOCKS):
         output_ticker = sorted_ticker_list[i]
         output_count = sorted_ticker_dict[output_ticker]
         stock_info = yf.Ticker(output_ticker).info
@@ -89,8 +122,10 @@ def insert_to_db(sorted_ticker_dict, ticker_dict):
         if not found_all_fields(stock_info):
             continue
 
+        ranking_change = get_ranking_change(prev_stock_list, output_ticker, to_DB)
+
         entry = {
-            'rank': len(toDB) + 1,
+            'rank': len(to_DB) + 1,
             'ticker': output_ticker,
             'name': ticker_dict[output_ticker],
             'industry': stock_info.get('industry', ''),
@@ -98,18 +133,18 @@ def insert_to_db(sorted_ticker_dict, ticker_dict):
             'previousClose': stock_info.get('previousClose', ''),
             'fiftyDayAverage': stock_info.get('fiftyDayAverage', ''),
             'averageDailyVolume10Day': stock_info.get('averageDailyVolume10Day', ''),
-            'timeStamp': now
+            'timeStamp': now,
+            'newOccur': ranking_change[0],
+            'rankingChange': ranking_change[1]
         }
 
-        toDB.append(entry)
+        to_DB.append(entry)
 
-        if len(toDB) == TOTAL_NUMBER_TO_PRESENT:
+        if len(to_DB) == TOTAL_NUMBER_TO_PRESENT:
             break
 
-    # print(toDB)
-    client = MongoClient(os.environ['DB_URL'])
-    collection = client['findMyGME']['stocks']
-    collection.insert_many(toDB)
+    # print(to_DB)
+    collection.insert_many(to_DB)
 
 
 def main():
@@ -127,10 +162,10 @@ def main():
 
     # Counts the appearances of tickers in the top500 hot posts in WSB
     # Filters out the abbreviations that generally do not represent stock tickers
-    # Also cuts down the size of the dictionary to 10
+    # Also cuts down the size of the dictionary to 20
     sorted_ticker_dict = count_tickers(reddit, ticker_dict)
 
-    # Inserts top10 tickers and the related financial information into the database
+    # Inserts Top 10 tickers and the related financial information into the database
     insert_to_db(sorted_ticker_dict, ticker_dict)
 
 
